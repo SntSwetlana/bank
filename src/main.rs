937ae5257667d3075b715b0f6fbf7b9236d09923
SntSwetlana <snt.swetlana@gmail.com>
2025-08-24 08:24:56 +0300

    return the owner

--- src/main.rs
+++ src/main.rs
@@ -25,13 +25,37 @@ impl Bank {
         Bank {     accounts: vec![] }
     }
 }
-fn println_account( account:Account){
+fn println_bank( bank:Bank) -> Bank {
+    println!("{:#?}", bank);
+    bank
+}
+fn println_account( account:Account) -> Account{
     println!("{:#?}", account);
+    account
+}
+
+
+fn println_accounts( accounts:Vec<Account>) -> Vec<Account>{
+    println!("{:#?}", accounts);
+    accounts
 }
 
 fn main() {
-    let bank = Bank::new();
-    let account = Account::new(1, String::from("Alice"));
-    println_account(account);
+    let mut bank = Bank::new();
+    let mut account = Account::new(1, String::from("Alice"));
+    account.balance = 100;
+    account = println_account(account);
+    account = println_account(account);
+
+
+    bank.accounts.push(Account::new(1, String::from("Alice")));
+    bank.accounts.push(Account::new(4, String::from("Bill")));
+    bank.accounts.push(Account::new(5, String::from("Ivy")));
+
+    bank.accounts = println_accounts(bank.accounts);
+    bank.accounts = println_accounts(bank.accounts);
+
+    println_bank(bank);
+    
 }
 
